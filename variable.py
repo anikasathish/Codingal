a70bdbec15bcc265435ec5076678cdf615bd9194
anikasathish <sathish.anika@gmail.com>
2025-11-08 17:17:03 -0600

    variable.py

---
+++ variable.py
@@ -0,0 +1,10 @@
+x= 6
+y= "hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"
+print(x)
+print(y)
+
+coding = "codingal"
+print(coding)
+
+name = input("Enter your name;")
+print("\nHello",name,"\nWelcome to the Coding World!")
