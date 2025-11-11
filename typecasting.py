94f53828907d0528da80ee36d05b92afcca7b3e2
anikasathish <sathish.anika@gmail.com>
2025-11-11 17:25:42 -0600

    typecasting.py

---
+++ typecasting.py
@@ -0,0 +1,19 @@
+name= "Anika Sathish"
+age= 11
+is_student= True
+weight= 80.000000001
+print("name:",name)
+print ("type of name:",type(name))
+print("age:",age)
+print("type of age:",type(age))
+print("is_student:",is_student)
+print("type of is_student:",type(is_student))
+print("weight:",weight)
+print("type of weight:",type(weight))
+print("\n after typecasting..")
+age=str(age)
+weight=int(weight)
+print("age:",age)
+print("type of age:",type(age))
+print("weight:",weight)
+print("type of weight:",type(weight))
