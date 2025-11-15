1dfbc14211c246c9df05306595ba33f0b5285354
anikasathish <sathish.anika@gmail.com>
2025-11-15 17:34:19 -0600

    percentage.py

---
+++ percentage.py
@@ -0,0 +1,11 @@
+math = int(input("maths ="))
+science = int(input("science =")) 
+english = int(input("english ="))
+socialstudies = int(input("social studies ="))
+
+sum = math + socialstudies + science + english
+
+percentage = (sum/400)*100
+print(" the percentage is:", percentage)
+
+
