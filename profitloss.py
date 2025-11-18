5835a907f6c115479fc3fb18197a4227cbd02f56
anikasathish <sathish.anika@gmail.com>
2025-11-18 17:18:59 -0600

    profitloss.py

---
+++ profitloss.py
@@ -0,0 +1,14 @@
+cp = float(input("enter your cost price:"))
+sp = float(input("enter your selling price:"))
+if cp<sp: 
+    profit = sp-cp
+    print("you made a profit!", profit)
+else:
+    loss = cp-sp
+    print("sorry your loss was:", loss)
+
+
+
+
+
+    
