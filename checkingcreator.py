ef23f2a2ef7360be24693512d58ab072f57298b4
anikasathish <sathish.anika@gmail.com>
2025-11-18 17:26:19 -0600

    checkingcreator.py

---
+++ checkingcreator.py
@@ -0,0 +1,9 @@
+num = int(input("enter a number: "))
+if num>15:
+    print(num," is greater than 15")
+    print("if block")
+else:
+    print(num," is smaller than 15")
+    print("else block")
+print("neither if nor else")
+
