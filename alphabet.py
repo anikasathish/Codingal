762fc4194d18c9471dbedea2dc19480cddc447d9
anikasathish <sathish.anika@gmail.com>
2025-11-22 18:13:17 -0600

    alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,6 @@
+character = str(input(" enter a character !"))
+if character.isalpha():
+    print("its an alphabet")
+else:
+    print("oh, its not an alphabet")
+    
--- bmi.py
+++ bmi.py
@@ -3,14 +3,14 @@ weight = float(input(" what is your weight ?"))
 BMI = weight/(height/100)**2
 print(" your bmi is ", BMI)
 if BMI <= 18.4:
-    print(" you are underweight !")
+    print(" you are underweight, eat more !")
 elif BMI <= 24.9:
-    print(" you are at a healthy bmi !")
+    print(" you are at a healthy bmi, this is the only way to be!")
 elif BMI <= 29.9:
-    print("you are overweight")
+    print("you are overweight go to the gym")
 elif BMI <= 34.9:
-    print(" you are severely overweight")
+    print(" you are severely overweight, get out of your house and excercise")
 elif BMI <= 39.9:
-    print(" you are obese, sorry.")
+    print(" you are obese, go lay on a hospital bed.")
 else:
-    print("you are severely obese")
+    print("you are severely obese, consider weight loss programs")
