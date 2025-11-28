5a59f604d471f7109a06dea73fe96db983be1ecc
anikasathish <sathish.anika@gmail.com>
2025-11-28 17:53:15 -0600

    swap.py

---
+++ swap.py
@@ -0,0 +1,5 @@
+a, b, c = 1, 2, 3 
+print("the original order of the numbers is:", a, b, c) 
+#now we will swap the numbers !
+a, b, c = c, a, b  
+print("the swapped order of the numbers is:", a, b, c)  
