8c33fdf79a45d68ff6b0dd068b45796fbda2e955
anikasathish <sathish.anika@gmail.com>
2026-01-23 18:20:29 -0600

    cccccccccccc

---
+++ boardturtleeeee.py
@@ -0,0 +1,28 @@
+import turtle
+
+turtle.Screen().bgcolor("Aqua")
+
+board = turtle.Turtle()
+
+board.forward(100)
+board.left(120) 
+board.forward(100)
+board.forward(100)
+board.left(120)
+board.pendown()
+board.right(150)
+board.forward(50)
+
+#secondstar
+
+
+board.pendown()
+board.right(90)
+board.forward(100)
+board.right(120)
+board.forward(100) 
+board.right(120)
+board.forward(100)
+
+turtle.done()
+
