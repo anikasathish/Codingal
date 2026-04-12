8caaea4c97c844e1d432f03504d087eb16fdd21d
anikasathish <sathish.anika@gmail.com>
2026-04-12 17:10:20 -0500

    dfdd

---
+++ break.py
@@ -0,0 +1,9 @@
+a = input("enter a word:")
+
+for i in a:
+    if i == "a":
+        print("found a")
+        break
+    else:
+        print("not found a")
+        
---
+++ pass.py
@@ -0,0 +1,10 @@
+for x in range(10):
+    if x % 20 == 0:
+        print("twist")
+    elif x % 15 ==0:
+        print("fizz")
+    elif x % 5==0:
+        print("buzz")
+    else:
+        print(x)
+        
